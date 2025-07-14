394357ecf146d1b9d9a36382eed9617827e9354f
Muneeb Shafiq <muneeb.shafiq47@gmail.com>
2025-07-14 21:19:20 +0200

    feat: Implemented users list with table display and logout functionality

--- src/app/features/users/users-list-page/users-list-page.component.ts
+++ src/app/features/users/users-list-page/users-list-page.component.ts
@@ -1,21 +1,29 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { UsersListComponent } from '../users-list/users-list.component';
 import { MatButton } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { UsersFacadeService } from '../../../core/facades/users-facade.service';
+import { AuthService } from '../../../core/services/auth.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-users-list-page',
   imports: [
     UsersListComponent,
-    MatButton
+    MatButton,
+    MatIconModule
   ],
   templateUrl: './users-list-page.component.html',
   styleUrl: './users-list-page.component.scss'
 })
-export class UsersListPageComponent {
+export class UsersListPageComponent implements OnInit {
   facade = inject(UsersFacadeService);
   router = inject(Router);
+  authService = inject(AuthService);
+
+  ngOnInit(): void {
+    this.facade.loadUsers();
+  }
 
   goToNew(): void {
     this.router.navigate(['/users/create']);
@@ -23,5 +31,10 @@ export class UsersListPageComponent {
 
   goToEdit(id: number): void {
     this.router.navigate(['/users', id]);
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth']);
   }
 }
--- src/app/features/users/users-list/users-list.component.ts
+++ src/app/features/users/users-list/users-list.component.ts
@@ -1,14 +1,29 @@
 import { Component, input, InputSignal, output, OutputEmitterRef } from '@angular/core';
 import { User } from '../../../shared/models/user';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
 export class UsersListComponent {
-  users = input<User[]>();
+  users = input<User[]>([]);
 
   edit: OutputEmitterRef<number> = output();
+
+  displayedColumns: string[] = ['id', 'username', 'role', 'actions'];
+
+  onEdit(userId: number): void {
+    this.edit.emit(userId);
+  }
 }
